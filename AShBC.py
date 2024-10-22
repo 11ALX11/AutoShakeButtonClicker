b0a2fbc844e782668d99af3ce44df8bade21bf79
Aleksey <11_alx_11@ro.ru>
2024-10-23 00:12:30 +0300

    trying to get pyautogui to work

--- AShBC.py
+++ AShBC.py
@@ -1,5 +1,17 @@
+from time import sleep
+import pyautogui
+
+
+TIME_BETWEEN_SCREENSHOTS = 1
+
+
 def main():
-    print()
+    pyautogui.PAUSE = TIME_BETWEEN_SCREENSHOTS
+    pyautogui.FAILSAFE = True
+
+    while True:
+        print( pyautogui.locateOnScreen("shake.png") )
+
 
 
 if __name__ == '__main__':
